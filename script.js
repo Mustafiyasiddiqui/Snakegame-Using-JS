0fe9291c2327e0020df2a3dbdc52d78fcaaf528d
mustafiya <mustafiyasheikh14@gmail.com>
2025-11-26 20:11:10 +0530

    Build snake movement logic and implement directional if-else controls

--- script.js
+++ script.js
@@ -5,8 +5,69 @@ const blockWidth = 30;
 const rows = Math.floor(board.clientHeight / blockHeight);
 const cols = Math.floor(board.clientWidth / blockWidth);
 
-for (let i = 0; i < rows * cols; i++) {
-  const block = document.createElement("div");
-  block.classList.add("block");
-  board.appendChild(block);
+const blocks = [];
+const snake = [
+  {
+    x: 1,
+    y: 3,
+  },
+  {
+    x: 1,
+    y: 4,
+  },
+  {
+    x: 1,
+    y: 5,
+  },
+];
+
+let direction = "down";
+
+// for (let i = 0; i < rows * cols; i++) {
+//   const block = document.createElement("div");
+//   block.classList.add("block");
+//   board.appendChild(block);
+// }
+
+for (let row = 0; row < rows; row++) {
+  for (let col = 0; col < cols; col++) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    board.appendChild(block);
+    block.innerText = `${row}-${col}`;
+    blocks[`${row}-${col}`] = block;
+  }
 }
+
+function render() {
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.add("fill");
+  });
+}
+
+setInterval(() => {
+  let head = null;
+  if (direction === "left") {
+    head = {
+      x: snake[0].x,
+      y: snake[0].y - 1,
+    };
+  } else if (direction === "right") {
+    head = {
+      x: snake[0].x,
+      y: snake[0].y + 1,
+    };
+  } else if (direction === "down") {
+    head = { x: snake[0].x + 1, y: snake[0].y };
+  } else if (direction === "up") {
+    head = { x: snake[0].x - 1, y: snake[0].y };
+  }
+
+  snake.forEach((segment) => {
+    blocks[`${segment.x}-${segment.y}`].classList.remove("fill");
+  });
+  snake.unshift(head);
+  snake.pop();
+
+  render();
+}, 300);
